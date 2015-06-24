13cc2ba4ee9d5e696989a36186da0f325651e109
Akhilesh Nirapure <akhilesh.nirapure@gmail.com>
2015-06-24 17:14:54 +0530

    added configuration for single app module

---
+++ SPDemo/tools/build.single.debug.js
@@ -0,0 +1,16 @@
+﻿({
+    //  baseUrl to consider while build, this overrides the baseUrl defined in config file
+    baseUrl: '../js/vendors',
+
+    //  defines all the shim configuration
+    mainConfigFile: '../js/common.js',
+    
+    //  The boostraper module which loads up the SPA 
+    name: 'app/pages/default',
+
+    //  the release output directory
+    out: '../Scripts/debug/app.js',
+
+    //  no minification
+	optimize: 'none'                     
+})
---
+++ SPDemo/tools/build.single.release.js
@@ -0,0 +1,17 @@
+﻿({
+    //  baseUrl to consider while build, this overrides the baseUrl defined in config file
+    baseUrl: '../js/vendors',
+
+    //  defines all the shim configuration
+    mainConfigFile: '../js/common.js',
+
+    //  The boostraper module which loads up the SPA
+    name: 'app/pages/default',
+
+    //  the release output directory
+    out: '../Scripts/release/app.js',
+    //  we want to generate sourcemaps hence we need version2
+    generateSourceMaps: true,
+    preserveLicenseComments: false,
+	optimize: 'uglify2'                     
+})
